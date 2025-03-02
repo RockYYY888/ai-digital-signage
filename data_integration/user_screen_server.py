82d0974abde0ef0cd93bba2a1ae806e37d7168a4
Zelin Xia <scyzx5@nottingham.edu.cn>
2025-03-02 16:34:24 +0800

    Add video selection mechanism

--- data_integration/user_screen_server.py
+++ data_integration/user_screen_server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+from data_interface import product_queue
 
 app = Flask(__name__,
             template_folder='templates',
@@ -7,6 +8,15 @@ app = Flask(__name__,
 @app.route('/')
 def index():
     return render_template('main_screen.html')
+
+@app.route('/get_product_queue', methods=['GET'])
+def get_product_queue():
+    """Return the contents of the product_queue as a list."""
+    if not product_queue.empty():  # Check if the queue is empty
+        product = product_queue.get()  # Retrieve the product name from the queue
+        return jsonify({"product": product})
+    else:
+        return jsonify({"product": None})  # If the queue is empty, return None
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5001)  # Run on port 5001
